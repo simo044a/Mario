f0c1a6f39d1f195d18f75017c1f76db41621d5f8
simo044a <91477508+simo044a@users.noreply.github.com>
2021-10-14 18:44:14 +0200

    Update Order.java
    Torsdags opdatering!

--- Mario/src/Order.java
+++ Mario/src/Order.java
@@ -3,21 +3,22 @@ import java.util.ArrayList;
 public class Order {
 
   private String orderName;
-  private String orderNr;
   private ArrayList<Pizza> orderArrayList = new ArrayList<>();
+
+  public Order(String orderName){
+    setOrderName(orderName);
+  }
 
   public void setOrderName(String orderName){this.orderName = orderName;}
   public String getOrderName(){return orderName;}
 
-  public void setOrderNr(String orderNr){this.orderNr = orderNr;}
-  public String getOrderNr(){return orderNr;}
 
   public void setOrderArrayList(ArrayList<Pizza> orderArrayList){this.orderArrayList = orderArrayList;}
   public ArrayList<Pizza> getOrderArrayList(){return orderArrayList;}
 
 
 public String toString(){
-    return orderName + orderArrayList;
+    return " " + orderName + " " + orderArrayList;
 }
 
 }
