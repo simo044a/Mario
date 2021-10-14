249ec088033a64a5317987d96acc38669c8ca690
simo044a <91477508+simo044a@users.noreply.github.com>
2021-10-14 18:43:12 +0200

    Update Pizza.java
    Torsdags opdatering!

--- Mario/src/Pizza.java
+++ Mario/src/Pizza.java
@@ -1,2 +1,27 @@
 public class Pizza {
+
+  private String pizzaName;
+  private int pizzaNum;
+  private double pizzaPrice;
+
+
+  Pizza(String pizzaName, int pizzaNum, double pizzaPrice){
+    setPizzaName(pizzaName);
+    setPizzaNum(pizzaNum);
+    setPizzaPrice(pizzaPrice);
+  }
+
+  public void setPizzaName(String pizzaName){this.pizzaName = pizzaName;}
+  public String getPizzaName(){return pizzaName;}
+
+  public void setPizzaNum(int pizzaNum){this.pizzaNum = pizzaNum;}
+  public int getPizzaNum(){return pizzaNum;}
+
+  public void setPizzaPrice(double pizzaPrice){this.pizzaPrice = pizzaPrice;}
+  public double getPizzaPrice(){return pizzaPrice;}
+
+public String toString(){
+return this.pizzaNum + " " + this.pizzaName + " " + this.pizzaPrice;
+}
+
 }
